567f595a6354fadf3b20ba1537a66680d2d4b375
Kenneth Nicholas <nicholas.kenneth.j@gmail.com>
2019-03-06 19:59:01 -0600

    loopsLab

--- loopsLab/src/loopsLab/Application.java
+++ loopsLab/src/loopsLab/Application.java
@@ -100,7 +100,9 @@ public class Application {
 		
 		System.out.println();
 		
-		for(int i = 0, size = 5; i < size; i++) {	
+		int size = 5;
+		
+		for(int i = 0; i < size; i++) {	
 			for(int j = 0; j < size; j++) {
 				if((i == j) || ((i + j) == (size - 1))) {
 					System.out.print("*");
